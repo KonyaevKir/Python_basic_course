5b24b5217e814c881ad666de6aebdd2b686c173b
Kirill Konyaev <Konyai94@icloud.com>
2022-06-19 23:57:45 +0300

    homework_01 is done

--- homework_01/main.py
+++ homework_01/main.py
@@ -21,8 +21,8 @@ EVEN = "even"
 PRIME = "prime"
 
 def is_prime(num):
-    # if num is None:
-    #     return False
+    if num == 0 or num == 1:
+        return False
     for n in range(2,int(num ** 0.5) + 1):
         if num % n == 0:
             return False
